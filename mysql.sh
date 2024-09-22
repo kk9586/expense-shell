c7e277445db97a99eee51d0f848565729057cc45
kk9586 <koteshkunapuri99@gmail.com>
2024-09-23 00:11:00 +0530

    Installing softwares

--- mysql.sh
+++ mysql.sh
@@ -46,6 +46,6 @@ VALIDATE $? "Enabled MySQL Server"
 systemctl start mysqld &>>$LOG_FILE
 VALIDATE $? "Started MySQL server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
-VALIDATE $? "setting root password"
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE
+VALIDATE $? "setting up root password"
 
