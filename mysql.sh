4bc74bc7dacbfaa18c33fe7ca42f54df826c066e
koteshkunapuri <koteshkunapuri@gmail.com>
2025-05-23 23:22:22 +0530

    creating database

--- mysql.sh
+++ mysql.sh
@@ -26,7 +26,7 @@ fi
 VALIDATE(){
        if [ $1 -eq 0 ]
           then  
-            echo -e "$2 is $G success $N .. check it" | tee -a $LOG_FILE
+            echo -e "$2 is $G success $N" | tee -a $LOG_FILE
             exit 1
           else 
             echo -e "$2 is $R failed. $N" | tee -a $LOG_FILE
