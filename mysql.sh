271bd01d597a595d9ff0f68182ecb68b71df7a2f
koteshkunapuri <koteshkunapuri@gmail.com>
2025-05-23 22:52:32 +0530

    creating database

--- mysql.sh
+++ mysql.sh
@@ -37,7 +37,7 @@ echo "script started executing at $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
-dnf install mysql-server -y &>> $LOG_FILE
+dnf install mysqld -y &>> $LOG_FILE
 VALIDATE $? "installing mysql server"
 
 systemctl enable mysqld
